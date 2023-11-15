f66c48a314525b3b94de8679ff07f6108a126378
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-11-15 14:39:46 +0900

    feat: Member 도메인 추가 (#16)

---
+++ src/main/java/com/flytrap/rssreader/domain/member/Member.java
@@ -0,0 +1,59 @@
+package com.flytrap.rssreader.domain.member;
+
+import com.flytrap.rssreader.infrastructure.entity.member.OauthServer;
+import java.time.Instant;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String profile;
+    private OauthInfo oauthInfo;
+    private Instant createdAt;
+
+    @Builder
+    private Member(Long id, String name, String email, String profile, long oauthPk, OauthServer oauthServer,
+        Instant createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.profile = profile;
+        this.oauthInfo = new OauthInfo(oauthPk, oauthServer);
+        this.createdAt = createdAt;
+    }
+
+    public static Member of(Long id, String name, String email, String profile, Long oauthPk, OauthServer oauthServer, Instant createdAt) {
+        return Member.builder()
+            .id(id)
+            .name(name)
+            .email(email)
+            .profile(profile)
+            .oauthPk(oauthPk)
+            .oauthServer(oauthServer)
+            .createdAt(createdAt)
+            .build();
+    }
+
+    public Long getOauthPk() {
+        return this.oauthInfo.getOauthPk();
+    }
+
+    public OauthServer getOauthServer() {
+        return this.oauthInfo.getOauthServer();
+    }
+}
+
+@Getter
+@AllArgsConstructor
+class OauthInfo {
+    private Long oauthPk;
+    private OauthServer oauthServer;
+}
