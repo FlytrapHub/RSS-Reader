ed8c123efaa1c3eae8ff1bf4127e7ff2908baf44
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-11-17 08:53:10 +0900

    chore: data.sql에 테스트용 Member, Subscribe 데이터 추가 (#24)

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+-- INSERT DATA
+INSERT INTO `member`(name, email, profile, oauth_pk, oauth_server, created_at)
+VALUES ('test', 'test@test.com', 'profile.png', '11111', 'GITHUB', '2023-11-16');
+
+INSERT INTO `rss_subscribe`(description, url, member_id, platform)
+VALUES ('description', 'https://v2.velog.io/rss/jinny-l', '1', 'VELOG');
