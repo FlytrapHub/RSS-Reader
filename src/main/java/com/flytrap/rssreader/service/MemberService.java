b12f9191028f60368628b6ce231ed3b53e81c03b
saehim <saeilee.vxv@gmail.com>
2023-11-17 14:53:45 +0900

    Feat: name으로 검색 service추가 (#22)

--- src/main/java/com/flytrap/rssreader/service/MemberService.java
+++ src/main/java/com/flytrap/rssreader/service/MemberService.java
@@ -5,6 +5,7 @@ import com.flytrap.rssreader.infrastructure.api.dto.UserResource;
 import com.flytrap.rssreader.infrastructure.entity.member.MemberEntity;
 import com.flytrap.rssreader.infrastructure.repository.MemberEntityJpaRepository;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,10 @@ public class MemberService {
     private final MemberEntityJpaRepository memberEntityJpaRepository;
 
     /**
+     * Member 정보를 반환합니다.
      * User resource가 Member 테이블에 존재하지 않으면 새로 가입 후 Member 정보를 반환합니다.
      * @param userResource
-     * @return
+     * @return Member domain
      */
     @Transactional
     public Member loginMember(UserResource userResource) {
@@ -27,7 +29,23 @@ public class MemberService {
             .toDomain();
     }
 
+    /**
+     * Member 테이블에 새로운 회원을 가입시킵니다.
+     * @param member
+     * @return
+     */
     private MemberEntity joinMember(Member member) {
         return memberEntityJpaRepository.save(MemberEntity.from(member));
+    }
+
+    /**
+     * 이름으로 회원을 검색합니다.
+     * @param name
+     * @return Member domain list
+     */
+    public List<Member> findByName(String name) {
+        return memberEntityJpaRepository.findAllByName(name).stream()
+                .map(MemberEntity::toDomain)
+                .toList();
     }
 }
