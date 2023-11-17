95d30f3f8b4789c9bc46addd35cbbd39aeb3284a
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-11-17 12:40:34 +0900

    chore: 필요없는 import 제거 (#24)

--- src/main/java/com/flytrap/rssreader/infrastructure/repository/SubscribeEntityJpaRepository.java
+++ src/main/java/com/flytrap/rssreader/infrastructure/repository/SubscribeEntityJpaRepository.java
@@ -1,6 +1,5 @@
 package com.flytrap.rssreader.infrastructure.repository;
 
-import com.flytrap.rssreader.infrastructure.entity.post.PostEntity;
 import com.flytrap.rssreader.infrastructure.entity.subscribe.SubscribeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
