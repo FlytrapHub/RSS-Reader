0b7b7d148507634dd05ba3eb826659c020901ff6
saehim <saeilee.vxv@gmail.com>
2023-11-18 02:10:32 +0900

    Refactor: filter exception handling 수정 (#40)
    - 그런데 구현하고보니 filter에서 잡지 않아도 되는 에러라 삭제할까 고민중입니다...

--- src/main/java/com/flytrap/rssreader/presentation/filter/ExceptionHandleFilter.java
+++ src/main/java/com/flytrap/rssreader/presentation/filter/ExceptionHandleFilter.java
@@ -1,13 +1,17 @@
 package com.flytrap.rssreader.presentation.filter;
 
+import com.flytrap.rssreader.global.exception.ApplicationException;
+import com.flytrap.rssreader.global.model.ErrorResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.security.sasl.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -22,11 +26,11 @@ public class ExceptionHandleFilter extends OncePerRequestFilter {
             FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (AuthenticationException e) {
             log.error(e.getMessage());
-
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
+            response.getWriter().println(ErrorResponse.occur("login", e).toString());
+            response.setHeader(HttpHeaders.ACCEPT_CHARSET, "utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         }
     }
 }
