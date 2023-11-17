869a0b76f0fb3ea0f7838d51eff7a797ee347380
saehim <saeilee.vxv@gmail.com>
2023-11-17 14:54:14 +0900

    Feat: name으로 검색 controller 수정 (#22)

--- src/main/java/com/flytrap/rssreader/presentation/controller/MemberController.java
+++ src/main/java/com/flytrap/rssreader/presentation/controller/MemberController.java
@@ -1,18 +1,31 @@
 package com.flytrap.rssreader.presentation.controller;
 
+import com.flytrap.rssreader.domain.member.Member;
 import com.flytrap.rssreader.global.model.ApplicationResponse;
+import com.flytrap.rssreader.presentation.dto.MemberSummary;
+import com.flytrap.rssreader.presentation.dto.NameSearch;
+import com.flytrap.rssreader.presentation.dto.NameSearch.Result;
+import com.flytrap.rssreader.service.MemberService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/members")
 public class MemberController {
 
-    @GetMapping
-    public ApplicationResponse<Object> searchMemberByName(String name) {
+    private final MemberService memberService;
 
-        return new ApplicationResponse<>(null);
+    @GetMapping
+    public ApplicationResponse<NameSearch.Result> searchMemberByName(NameSearch nameSearch) {
+        List<Member> results = memberService.findByName(nameSearch.name());
+
+        List<MemberSummary> memberSummaries = results.stream().map(MemberSummary::from).toList();
+        return new ApplicationResponse<>(new Result(memberSummaries));
     }
 
 }
