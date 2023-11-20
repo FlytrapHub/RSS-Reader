ffe7d1d838d3aca84beb3bec54bfde074b7f3d10
saehim <saeilee.vxv@gmail.com>
2023-11-20 18:57:42 +0900

    Feat: folder 삭제 soft delete 구현 (#49)

--- src/main/java/com/flytrap/rssreader/service/FolderService.java
+++ src/main/java/com/flytrap/rssreader/service/FolderService.java
@@ -1,12 +1,15 @@
 package com.flytrap.rssreader.service;
 
 import com.flytrap.rssreader.domain.folder.Folder;
+import com.flytrap.rssreader.domain.folder.SharedStatus;
 import com.flytrap.rssreader.global.exception.NoSuchDomainException;
 import com.flytrap.rssreader.global.exception.NotBelongToMemberException;
 import com.flytrap.rssreader.infrastructure.entity.folder.FolderEntity;
 import com.flytrap.rssreader.infrastructure.repository.FolderEntityJpaRepository;
 import com.flytrap.rssreader.presentation.dto.FolderRequest.CreateRequest;
 import jakarta.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,16 +27,14 @@ public class FolderService {
 
     @Transactional
     public Folder updateFolder(CreateRequest request, long folderId, long memberId) {
-        FolderEntity folderEntity = repository.findById(folderId)
+        FolderEntity folderEntity = repository.findByIdAndIsDeletedFalse(folderId)
                 .orElseThrow(() -> new NoSuchDomainException(Folder.class));
         verifyBelongTo(memberId, folderEntity);
 
         Folder folder = folderEntity.toDomain();
         folder.updateName(request.name());
 
-        repository.save(FolderEntity.from(folder));
-
-        return folder;
+        return repository.save(FolderEntity.from(folder)).toDomain();
     }
 
     private void verifyBelongTo(long memberId, FolderEntity folderEntity) {
@@ -41,4 +42,24 @@ public class FolderService {
             throw new NotBelongToMemberException(folderEntity.toDomain());
         }
     }
+
+    @Transactional
+    public Folder deleteFolder(Long folderId, long id) {
+        FolderEntity folderEntity = repository.findByIdAndIsDeletedFalse(folderId)
+                .orElseThrow(() -> new NoSuchDomainException(Folder.class));
+        verifyBelongTo(id, folderEntity);
+
+        Folder folder = folderEntity.toDomain();
+        folder.delete();
+
+        return repository.save(FolderEntity.from(folder)).toDomain();
+    }
+
+    @Transactional(readOnly = true)
+    public Map<SharedStatus, Folder> findAllByMemberIdGroupByShared(long memberId) {
+        return repository.findAllByMemberIdAndIsDeletedFalse(memberId).stream()
+                .map(FolderEntity::toDomain)
+                .collect(Collectors.toMap(Folder::getSharedStatus, folder -> folder));
+    }
+
 }
