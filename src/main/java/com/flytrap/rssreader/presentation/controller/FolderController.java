0a74c3d3823831b859bbff04f06c514bf6b03a12
saehim <saeilee.vxv@gmail.com>
2023-11-20 20:29:45 +0900

    Feat: folder 삭제 controller 구현 (#49)

--- src/main/java/com/flytrap/rssreader/presentation/controller/FolderController.java
+++ src/main/java/com/flytrap/rssreader/presentation/controller/FolderController.java
@@ -8,11 +8,14 @@ import com.flytrap.rssreader.presentation.dto.FolderRequest;
 import com.flytrap.rssreader.service.FolderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,5 +44,16 @@ public class FolderController {
         Folder updatedFolder = folderService.updateFolder(request, folderId, member.id());
 
         return new ApplicationResponse<>(FolderRequest.Response.from(updatedFolder));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{folderId}")
+    public ApplicationResponse<Void> deleteFolder(
+            @PathVariable Long folderId,
+            @Login SessionMember member) {
+
+        folderService.deleteFolder(folderId, member.id());
+
+        return new ApplicationResponse<>(null);
     }
 }
