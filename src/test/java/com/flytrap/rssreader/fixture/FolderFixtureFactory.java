6413377f8beaa1b493859ac95837415ae2b12c91
saehim <saeilee.vxv@gmail.com>
2023-11-20 15:51:04 +0900

    Feat: Folder fixture factory 구현 (#49)

---
+++ src/test/java/com/flytrap/rssreader/fixture/FolderFixtureFactory.java
@@ -0,0 +1,31 @@
+package com.flytrap.rssreader.fixture;
+
+import com.flytrap.rssreader.domain.folder.Folder;
+import com.flytrap.rssreader.domain.member.Member;
+import com.flytrap.rssreader.infrastructure.entity.folder.FolderEntity;
+
+public class FolderFixtureFactory {
+
+    public static class FolderFields {
+        public static Long id = 1L;
+        public static String name = "folderName";
+        public static Member member = FixtureFactory.generateMember();
+    }
+
+    public static FolderEntity generateFolderEntity() {
+        return FolderEntity.builder()
+                .id(FolderFields.id)
+                .name(FolderFields.name)
+                .memberId(FolderFields.member.getId())
+                .build();
+    }
+
+    public static Folder generateFolder() {
+        return Folder.builder()
+                .id(FolderFields.id)
+                .name(FolderFields.name)
+                .memberId(FolderFields.member.getId())
+                .build();
+    }
+
+}
