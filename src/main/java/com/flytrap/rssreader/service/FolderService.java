e38c7dabefd4bc688dff9e61df0ccec2c7804f5b
saehim <saeilee.vxv@gmail.com>
2023-11-20 15:48:45 +0900

    Feat: 폴더 이름 수정 service 구현 (#49)

--- src/main/java/com/flytrap/rssreader/service/FolderService.java
+++ src/main/java/com/flytrap/rssreader/service/FolderService.java
@@ -1,12 +1,15 @@
 package com.flytrap.rssreader.service;
 
 import com.flytrap.rssreader.domain.folder.Folder;
-import com.flytrap.rssreader.domain.member.Member;
+import com.flytrap.rssreader.global.exception.NoSuchDomainException;
+import com.flytrap.rssreader.global.exception.NotBelongToMemberException;
 import com.flytrap.rssreader.infrastructure.entity.folder.FolderEntity;
 import com.flytrap.rssreader.infrastructure.repository.FolderEntityJpaRepository;
-import com.flytrap.rssreader.presentation.dto.FolderCreate;
+import com.flytrap.rssreader.presentation.dto.FolderRequest.CreateRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +17,25 @@ public class FolderService {
 
     private final FolderEntityJpaRepository repository;
 
-    public Folder createNewFolder(FolderCreate request, long member) {
+    public Folder createNewFolder(@Valid CreateRequest request, long member) {
         Folder folder = Folder.create(request.name(), member);
         return repository.save(FolderEntity.from(folder)).toDomain();
     }
+
+    @Transactional
+    public Folder updateFolder(CreateRequest request, long folderId, long memberId) {
+        FolderEntity folderEntity = repository.findById(folderId)
+                .orElseThrow(() -> new NoSuchDomainException(Folder.class));
+        verifyBelongTo(memberId, folderEntity);
+
+        Folder folder = folderEntity.toDomain();
+        folder.updateName(request.name());
+
+        repository.save(FolderEntity.from(folder));
+
+        return folder;
+    }
+
     private void verifyBelongTo(long memberId, FolderEntity folderEntity) {
         if (folderEntity.getMemberId() != memberId) {
             throw new NotBelongToMemberException(folderEntity.toDomain());
