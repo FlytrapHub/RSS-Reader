9a452329e53790f43697cbbf1b1308541286d2be
gamja <leekhy02@gmail.com>
2023-11-20 12:34:39 +0900

    Refactor: return 값을 MemberDto를 반환하도록 변경 (#33)

--- src/main/java/com/flytrap/rssreader/service/AuthService.java
+++ src/main/java/com/flytrap/rssreader/service/AuthService.java
@@ -4,6 +4,7 @@ import com.flytrap.rssreader.domain.member.Member;
 import com.flytrap.rssreader.infrastructure.api.AuthProvider;
 import com.flytrap.rssreader.infrastructure.properties.AuthProperties;
 import com.flytrap.rssreader.presentation.dto.Login;
+import com.flytrap.rssreader.presentation.dto.SessionMember;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,13 +20,14 @@ public class AuthService {
 
     public Member doAuthentication(Login request) {
         return authProvider.requestAccessToken(request.code())
-            .flatMap(authProvider::requestUserResource)
-            .map(memberService::loginMember)
-            .block();
+                .flatMap(authProvider::requestUserResource)
+                .map(memberService::loginMember)
+                .block();
     }
 
-    public void login(Member member, HttpSession session) {
-        session.setAttribute(authProperties.sessionId(), member);
+    public SessionMember login(Member member, HttpSession session) {
+        session.setAttribute(authProperties.sessionId(), SessionMember.from(member));
+        return SessionMember.from(member);
     }
 
     public void logout(HttpSession session) {
