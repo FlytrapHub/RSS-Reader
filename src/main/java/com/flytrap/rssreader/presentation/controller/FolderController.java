c57b4b1cd221784e1dc367e73684085aa1020123
saehim <saeilee.vxv@gmail.com>
2023-11-20 10:03:10 +0900

    Refactor: 헷갈리는 DTO 시그니처 변경 (#49)

--- src/main/java/com/flytrap/rssreader/presentation/controller/FolderController.java
+++ src/main/java/com/flytrap/rssreader/presentation/controller/FolderController.java
@@ -1,14 +1,15 @@
 package com.flytrap.rssreader.presentation.controller;
 
 import com.flytrap.rssreader.domain.folder.Folder;
-import com.flytrap.rssreader.domain.member.Member;
 import com.flytrap.rssreader.global.model.ApplicationResponse;
 import com.flytrap.rssreader.presentation.dto.SessionMember;
 import com.flytrap.rssreader.presentation.resolver.Login;
-import com.flytrap.rssreader.presentation.dto.FolderCreate;
+import com.flytrap.rssreader.presentation.dto.FolderRequest;
 import com.flytrap.rssreader.service.FolderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,13 +23,13 @@ public class FolderController {
     private final FolderService folderService;
 
     @PostMapping
-    public ApplicationResponse<FolderCreate.Response> createFolder(
-            @Valid @RequestBody FolderCreate request,
+    public ApplicationResponse<FolderRequest.Response> createFolder(
+            @Valid @RequestBody FolderRequest.CreateRequest request,
             @Login SessionMember member) {
 
         Folder newFolder = folderService.createNewFolder(request, member.id());
 
-        return new ApplicationResponse<>(FolderCreate.Response.from(newFolder));
+        return new ApplicationResponse<>(FolderRequest.Response.from(newFolder));
     }
 
 }
