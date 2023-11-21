54b87719bc288bbf676fe22de1fe41c874ceb2cb
gamja <leekhy02@gmail.com>
2023-11-21 17:36:24 +0900

    feat:  subscribe,구독취소 메서드 추가 (#25)

--- src/main/java/com/flytrap/rssreader/service/SubscribeService.java
+++ src/main/java/com/flytrap/rssreader/service/SubscribeService.java
@@ -1,5 +1,10 @@
 package com.flytrap.rssreader.service;
 
+import com.flytrap.rssreader.domain.subscribe.Subscribe;
+import com.flytrap.rssreader.infrastructure.api.RssChecker;
+import com.flytrap.rssreader.infrastructure.repository.SubscribeEntityJpaRepository;
+import com.flytrap.rssreader.presentation.dto.SessionMember;
+import com.flytrap.rssreader.presentation.dto.SubscribeRequest.CreateRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,5 +20,16 @@ public class SubscribeService {
     // 새로 구독하지말고 DB에있는 url(정보를) 가져다써라
     //  step3하지만 새로운 블로그라면 새로 추가해주자
     //  새로운 구독이라면 DB 관계형테이블 구독 에 넣어라
+    private final SubscribeEntityJpaRepository subscribeJpaRepository;
+    private final RssChecker rssChecker;
 
+    public Subscribe subscribe(CreateRequest request, Long folderId, long id) {
+        rssChecker.checker(request);
+        return Subscribe.builder()
+                .build();
+    }
+
+    public void unsubscribe(Long folderId, Long subscribeId, SessionMember member) {
+
+    }
 }
