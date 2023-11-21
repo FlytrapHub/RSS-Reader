8f872f7ec143a897a3b53b0676e5222430dc489b
gamja <leekhy02@gmail.com>
2023-11-21 15:11:52 +0900

    feat: 구독 컨트롤러,레코드 구현 (#25)

--- src/main/java/com/flytrap/rssreader/presentation/controller/FolderUpdateController.java
+++ src/main/java/com/flytrap/rssreader/presentation/controller/FolderUpdateController.java
@@ -1,11 +1,14 @@
 package com.flytrap.rssreader.presentation.controller;
 
 import com.flytrap.rssreader.domain.folder.Folder;
+import com.flytrap.rssreader.domain.subscribe.Subscribe;
 import com.flytrap.rssreader.global.model.ApplicationResponse;
 import com.flytrap.rssreader.presentation.dto.SessionMember;
+import com.flytrap.rssreader.presentation.dto.SubscribeRequest;
 import com.flytrap.rssreader.presentation.resolver.Login;
 import com.flytrap.rssreader.presentation.dto.FolderRequest;
 import com.flytrap.rssreader.service.FolderUpdateService;
+import com.flytrap.rssreader.service.SubscribeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FolderUpdateController {
 
     private final FolderUpdateService folderService;
+    private final SubscribeService subscribeService;
 
     @PostMapping
     public ApplicationResponse<FolderRequest.Response> createFolder(
@@ -53,6 +57,30 @@ public class FolderUpdateController {
             @Login SessionMember member) {
 
         folderService.deleteFolder(folderId, member.id());
+
+        return new ApplicationResponse<>(null);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{folderId}/rss")
+    public ApplicationResponse<SubscribeRequest.Response> subscribe(
+            @PathVariable Long folderId,
+            @Valid @RequestBody SubscribeRequest.CreateRequest request,
+            @Login SessionMember member) {
+
+        Subscribe subscribe = subscribeService.subscribe(request, folderId, member.id());
+
+        return new ApplicationResponse<>(SubscribeRequest.Response.from(subscribe));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{folderId}/rss/{subscribeId}")
+    public ApplicationResponse<Void> unsubscribe(
+            @PathVariable Long folderId,
+            @PathVariable Long subscribeId,
+            @Login SessionMember member) {
+
+        subscribeService.unsubscribe(folderId, subscribeId, member);
 
         return new ApplicationResponse<>(null);
     }
