ad95d1d78b0989e28d0f463ce75f85ca1ff630bd
gamja <leekhy02@gmail.com>
2023-11-22 17:51:18 +0900

    feat: url의 존재 여부를 체크하기 위한 쿼리메서드 추가  (#25)

--- src/main/java/com/flytrap/rssreader/infrastructure/repository/SubscribeEntityJpaRepository.java
+++ src/main/java/com/flytrap/rssreader/infrastructure/repository/SubscribeEntityJpaRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscribeEntityJpaRepository extends JpaRepository<SubscribeEntity, Long>  {
 
+    boolean existsByUrl(String url);
 }
