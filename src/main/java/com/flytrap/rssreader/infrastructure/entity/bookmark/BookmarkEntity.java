9eba8fff4556ab92f1e302775ac9ec70520ad076
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-11-24 05:16:41 +0900

    feat: Bookmark, Open Entity 추가 (#64)

---
+++ src/main/java/com/flytrap/rssreader/infrastructure/entity/bookmark/BookmarkEntity.java
@@ -0,0 +1,41 @@
+package com.flytrap.rssreader.infrastructure.entity.bookmark;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "bookmark")
+public class BookmarkEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Builder
+    protected BookmarkEntity(Long id, Long memberId, Long postId) {
+        this.id = id;
+        this.memberId = memberId;
+        this.postId = postId;
+    }
+
+    public boolean isSameMember(Long memberId) {
+        return Objects.equals(this.memberId, memberId);
+    }
+}
