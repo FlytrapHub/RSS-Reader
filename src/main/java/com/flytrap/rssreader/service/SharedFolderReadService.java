8be9759fb48a7ed9b54f04daec58118b45787e59
saehim <saeilee.vxv@gmail.com>
2023-11-24 14:20:25 +0900

    Feat: 폴더 Id마다 멤버 리스트를 반환하도록 수정 (#66)
    - 중복 로직 삭제
    - 다른 레포지토리를 의존하지 않도록 수정

--- src/main/java/com/flytrap/rssreader/service/SharedFolderReadService.java
+++ src/main/java/com/flytrap/rssreader/service/SharedFolderReadService.java
@@ -1,17 +1,11 @@
 package com.flytrap.rssreader.service;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.mapping;
-import static java.util.stream.Collectors.toList;
-
-import com.flytrap.rssreader.domain.folder.Folder;
-import com.flytrap.rssreader.domain.member.Member;
-import com.flytrap.rssreader.infrastructure.entity.folder.FolderEntity;
 import com.flytrap.rssreader.infrastructure.entity.shared.SharedFolderEntity;
-import com.flytrap.rssreader.infrastructure.repository.FolderEntityJpaRepository;
 import com.flytrap.rssreader.infrastructure.repository.SharedFolderJpaRepository;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,41 +15,43 @@ import org.springframework.transaction.annotation.Transactional;
 public class SharedFolderReadService {
 
     private SharedFolderJpaRepository sharedFolderJpaRepository;
-    private FolderEntityJpaRepository folderEntityJpaRepository;
 
+    /**
+     * 멤버Id로 초대된 폴더Id 목록을 반환합니다.
+     * @param memberId
+     * @return List<FolderId>
+     */
     @Transactional(readOnly = true)
-    public List<Folder> findFoldersInvited(long memberId) {
+    public List<Long> findFoldersInvited(long memberId) {
         List<SharedFolderEntity> shared = sharedFolderJpaRepository.findAllByMemberId(
                 memberId);
 
-        List<Long> idList = shared.stream()
+        return shared.stream()
                 .map(SharedFolderEntity::getFolderId)
                 .toList();
-
-        return folderEntityJpaRepository.findAllById(idList).stream()
-                .map(FolderEntity::toDomain)
-                .toList();
     }
 
+    /**
+     * 폴더Id와 멤버Id로 초대 여부를 반환합니다.
+     * @param folderId
+     * @param memberId
+     */
     @Transactional(readOnly = true)
-    public Map<Long, List<Member>> findAllInvited(long id) {
-        List<SharedFolderEntity> sharedFolderEntities =
-                sharedFolderJpaRepository.findAllByMemberId(id);
-
-        return sharedFolderEntities.stream()
-                .collect(groupingBy(SharedFolderEntity::getFolderId,
-                        mapping(e -> e.getMember().toDomain(), toList())));
-    }
-
-    @Transactional(readOnly = true)
-    public Folder findFolderInvited(long folderId, long memberId) {
-
+    public void verifyFolderInvited(long folderId, long memberId) {
         if (!sharedFolderJpaRepository.existsByFolderIdAndMemberId(folderId, memberId)) {
             throw new IllegalArgumentException("공유된 폴더가 아닙니다.");
         }
+    }
 
-        return folderEntityJpaRepository.findById(folderId)
-                .map(FolderEntity::toDomain)
-                .orElseThrow();
+    /**
+     * 폴더Id마다 초대된 멤버리스트 Map을 반환합니다.
+     * @param invitedFolderIds
+     * @return Map<FolderId, List<MemberId>>
+     */
+    @Transactional(readOnly = true)
+    public Map<Long, List<Long>> findMembersInFolders(List<Long> invitedFolderIds) {
+        return sharedFolderJpaRepository.findAllByFolderIdIn(invitedFolderIds).stream()
+                .collect(Collectors.groupingBy(SharedFolderEntity::getFolderId,
+                        Collectors.mapping(SharedFolderEntity::getMemberId, Collectors.toList())));
     }
 }
