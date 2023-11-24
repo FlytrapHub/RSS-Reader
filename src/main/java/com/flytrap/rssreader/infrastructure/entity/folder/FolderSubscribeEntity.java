fc286c2530306f3d6679012dcf78574b0526b0a7
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-11-24 18:23:27 +0900

    fix: 스키마, 데이터 엔티티 수정 (#68)

--- src/main/java/com/flytrap/rssreader/infrastructure/entity/folder/FolderSubscribeEntity.java
+++ src/main/java/com/flytrap/rssreader/infrastructure/entity/folder/FolderSubscribeEntity.java
@@ -1,11 +1,13 @@
 package com.flytrap.rssreader.infrastructure.entity.folder;
 
+import com.flytrap.rssreader.domain.subscribe.Subscribe;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Id;
@@ -25,4 +27,23 @@ public class FolderSubscribeEntity {
 
     @Column(name = "subscribe_id", nullable = false)
     private Long subscribeId;
+
+    @Column(length = 2500, nullable = false)
+    private String description;
+
+    @Builder
+    public FolderSubscribeEntity(Long id, Long folderId, Long subscribeId, String description) {
+        this.id = id;
+        this.folderId = folderId;
+        this.subscribeId = subscribeId;
+        this.description = description;
+    }
+
+    public static FolderSubscribeEntity from(Subscribe subscribe, Long folderId) {
+        return FolderSubscribeEntity.builder()
+            .subscribeId(subscribe.getId())
+            .folderId(folderId)
+            .description(subscribe.getDescription())
+            .build();
+    }
 }
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS `rss_subscribe`
 (
     `id`          bigint        NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `title` varchar(2500) NOT NULL,
-    `description` varchar(2500) NOT NULL,
     `url`         varchar(2500) NOT NULL,
     `platform`    varchar(25)   NOT NULL
 );
@@ -45,7 +44,8 @@ CREATE TABLE IF NOT EXISTS `folder_subscribe`
 (
     `id`                 bigint     NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `subscribe_id`      bigint      NOT NULL,
-    `folder_id`         bigint      NOT NULL
+    `folder_id`         bigint      NOT NULL,
+    `description`       varchar(255)      NOT NULL
  );
 
 CREATE TABLE IF NOT EXISTS `folder_member`
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `folder_member`
  );
 
 CREATE TABLE `bookmark` (
-    `id`	VARCHAR(255)	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `id`	    bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `member_id`	bigint	NOT NULL,
     `post_id`	bigint	NOT NULL
 );
