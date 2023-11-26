64c9e8823b6c43649ccda07f361d838e8e051076
gamja <leekhy02@gmail.com>
2023-11-26 18:46:30 +0900

    feat: response dto 추가 (#76)

--- src/main/java/com/flytrap/rssreader/presentation/dto/SubscribeRequest.java
+++ src/main/java/com/flytrap/rssreader/presentation/dto/SubscribeRequest.java
@@ -2,6 +2,7 @@ package com.flytrap.rssreader.presentation.dto;
 
 import com.flytrap.rssreader.domain.subscribe.Subscribe;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 
 /**
  * 로그인 후 Rss구독할 URL입니다.
@@ -17,6 +18,13 @@ public record SubscribeRequest() {
         public static SubscribeRequest.Response from(
                 Subscribe subscribe) {
             return new SubscribeRequest.Response(subscribe.getId());
+        }
+    }
+
+    public record ResponseList(List<Subscribe> subscribeList) {
+
+        public static SubscribeRequest.ResponseList from(List<Subscribe> subscribeList) {
+            return new SubscribeRequest.ResponseList(subscribeList);
         }
     }
 }
