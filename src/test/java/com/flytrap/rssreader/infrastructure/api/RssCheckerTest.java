3ca74a77cee3c001efa90cd30baf0c79f6922f3f
gamja <leekhy02@gmail.com>
2023-11-26 16:28:27 +0900

    test: 피드백 반영 테스트 코드 합치기 (#76)

--- src/test/java/com/flytrap/rssreader/infrastructure/api/RssCheckerTest.java
+++ src/test/java/com/flytrap/rssreader/infrastructure/api/RssCheckerTest.java
@@ -2,11 +2,14 @@ package com.flytrap.rssreader.infrastructure.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.flytrap.rssreader.domain.subscribe.BlogPlatform;
 import com.flytrap.rssreader.presentation.dto.RssFeedData;
 import com.flytrap.rssreader.presentation.dto.SubscribeRequest.CreateRequest;
 import java.util.Optional;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,13 +33,15 @@ class RssCheckerTest {
         Optional<RssFeedData> result = rssChecker.parseRssDocuments(createRequest);
         BlogPlatform blogPlatform = BlogPlatform.parseLink(createRequest.blogUrl());
 
-        System.out.println("result = " + result);
         // then
-        assertTrue(result.isPresent());
-        assertEquals(createRequest.blogUrl(), result.orElseThrow().url());
-        Assertions.assertNotNull(result.get().title());
-        Assertions.assertNotNull(result.get().description());
-        Assertions.assertNotNull(result.get().platform());
-        Assertions.assertEquals(blogPlatform, result.get().platform());
+
+        SoftAssertions.assertSoftly(softAssertions -> {
+            assertTrue(true);
+            assertEquals(createRequest.blogUrl(), result.orElseThrow().url());
+            Assertions.assertNotNull(result.get().title());
+            Assertions.assertNotNull(result.get().description());
+            Assertions.assertNotNull(result.get().platform());
+            Assertions.assertEquals(blogPlatform, result.get().platform());
+        });
     }
 }
