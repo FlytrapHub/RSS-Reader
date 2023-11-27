6ec07b15011114a9712d8c8a125b9ba02524a932
saehim <saeilee.vxv@gmail.com>
2023-11-27 12:03:00 +0900

    Style: 파라미터 순서 변경 (#69)

--- src/main/java/com/flytrap/rssreader/service/dto/PostOpenParam.java
+++ src/main/java/com/flytrap/rssreader/service/dto/PostOpenParam.java
@@ -2,13 +2,8 @@ package com.flytrap.rssreader.service.dto;
 
 import com.flytrap.rssreader.infrastructure.entity.post.OpenEntity;
 
-public record PostOpenParam(long postId,
-                            long memberId) {
-
-    public static PostOpenParam of(long postId,
-                                   long memberId) {
-        return new PostOpenParam(postId, memberId);
-    }
+public record PostOpenParam(long memberId,
+                            long postId) {
 
     public OpenEntity toEntity() {
         return OpenEntity.builder()
