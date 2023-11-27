09cca11fadb9056c7719bb7283fb4a04067e3852
gamja <leekhy02@gmail.com>
2023-11-28 02:42:10 +0900

    test: add 하는  테스트 코드구현 (#76)

---
+++ src/test/java/com/flytrap/rssreader/service/ReactionServiceTest.java
@@ -0,0 +1,62 @@
+package com.flytrap.rssreader.service;
+
+import static com.flytrap.rssreader.fixture.FixtureFactory.generateMemberEntity;
+import static com.flytrap.rssreader.fixture.FixtureFactory.generatePostEntity;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.flytrap.rssreader.infrastructure.entity.member.MemberEntity;
+import com.flytrap.rssreader.infrastructure.entity.post.PostEntity;
+import com.flytrap.rssreader.infrastructure.entity.reaction.ReactionEntity;
+import com.flytrap.rssreader.infrastructure.repository.MemberEntityJpaRepository;
+import com.flytrap.rssreader.infrastructure.repository.PostEntityJpaRepository;
+import com.flytrap.rssreader.infrastructure.repository.ReactionEntityJpaRepository;
+import java.util.Optional;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@DisplayName("ReactionServic 서비스 로직 단위 테스트")
+@ExtendWith(MockitoExtension.class)
+public class ReactionServiceTest {
+
+    @Mock
+    PostEntityJpaRepository postRepository;
+
+    @Mock
+    MemberEntityJpaRepository memberRepository;
+
+    @Mock
+    ReactionEntityJpaRepository reactionRepository;
+
+    @InjectMocks
+    ReactionService reactionService;
+
+    @Test
+    @DisplayName("POST에 이모지 리액션 달기 성공.")
+    void post_add_reaction_success() {
+
+        // given
+        MemberEntity member = generateMemberEntity();
+        PostEntity post = generatePostEntity(1L);
+
+        // when
+        when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
+        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
+        when(reactionRepository.save(Mockito.any(ReactionEntity.class)))
+                .thenAnswer(i -> i.getArguments()[0]);
+        reactionService.addReaction(post.getId(), member.getId(), "👍");
+
+        // then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            verify(reactionRepository, times(1)).save(any());
+        });
+    }
+}
