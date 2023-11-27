ce2f5e0c6c122170c8502a69e69b657bf99c3beb
saehim <saeilee.vxv@gmail.com>
2023-11-27 14:59:40 +0900

    Fix: spel이 사용할 정적 메서드 추가 (#69)

--- src/main/java/com/flytrap/rssreader/service/dto/PostOpenParam.java
+++ src/main/java/com/flytrap/rssreader/service/dto/PostOpenParam.java
@@ -2,8 +2,12 @@ package com.flytrap.rssreader.service.dto;
 
 import com.flytrap.rssreader.infrastructure.entity.post.OpenEntity;
 
-public record PostOpenParam(long memberId,
-                            long postId) {
+public record PostOpenParam(Long memberId,
+                            Long postId) {
+
+    public static PostOpenParam create(Long memberId, Long postId) {
+        return new PostOpenParam(memberId, postId);
+    }
 
     public OpenEntity toEntity() {
         return OpenEntity.builder()
