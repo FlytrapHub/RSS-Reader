f3aebe202f191cbb0b13c5463130497f3dd7786a
gamja <leekhy02@gmail.com>
2023-11-30 00:54:14 +0900

    feat: 삭제를 위한 쿼리 추가 (#92)

--- src/main/java/com/flytrap/rssreader/infrastructure/repository/AlertEntityJpaRepository.java
+++ src/main/java/com/flytrap/rssreader/infrastructure/repository/AlertEntityJpaRepository.java
@@ -1,7 +1,10 @@
 package com.flytrap.rssreader.infrastructure.repository;
 
 import com.flytrap.rssreader.infrastructure.entity.alert.AlertEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlertEntityJpaRepository extends JpaRepository<AlertEntity, Long> {
+
+    Optional<AlertEntity> findByFolderIdAndMemberId(Long folderId, Long memberId);
 }
