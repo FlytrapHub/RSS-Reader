94a78c17b0cbcf617c26c877c85d5c44e9a21ad7
gamja <leekhy02@gmail.com>
2023-12-03 18:49:25 +0900

    feat: Post서비스에 큐를 추가하는 의존성 추가 (#92)

--- src/main/java/com/flytrap/rssreader/service/PostCollectService.java
+++ src/main/java/com/flytrap/rssreader/service/PostCollectService.java
@@ -1,5 +1,6 @@
 package com.flytrap.rssreader.service;
 
+import com.flytrap.rssreader.domain.alert.q.SubscribeEventPublisher;
 import com.flytrap.rssreader.infrastructure.api.RssPostParser;
 import com.flytrap.rssreader.infrastructure.api.dto.RssSubscribeResource;
 import com.flytrap.rssreader.infrastructure.api.dto.RssSubscribeResource.RssItemResource;
@@ -7,6 +8,7 @@ import com.flytrap.rssreader.infrastructure.entity.post.PostEntity;
 import com.flytrap.rssreader.infrastructure.entity.subscribe.SubscribeEntity;
 import com.flytrap.rssreader.infrastructure.repository.PostEntityJpaRepository;
 import com.flytrap.rssreader.infrastructure.repository.SubscribeEntityJpaRepository;
+import com.flytrap.rssreader.service.alert.AlertFacadeService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +28,8 @@ public class PostCollectService {
     private final RssPostParser postParser;
     private final SubscribeEntityJpaRepository subscribeEntityJpaRepository;
     private final PostEntityJpaRepository postEntityJpaRepository;
-
+    private final SubscribeEventPublisher publisher;
+    private final AlertFacadeService alertFacadeService;
     private final TaskExecutor taskExecutor;
 
     @Scheduled(fixedDelay = TEN_MINUTE)
@@ -36,30 +39,34 @@ public class PostCollectService {
         for (SubscribeEntity subscribe : subscribes) {
             processPostCollectionAsync(subscribe);
         }
+        //TODO 스케줄 작업이 끝나면 알림 envet작업이 발생한다. 큐에 담긴 이벤트를 발행한다.
+        log.info("alert 실행");
+        alertFacadeService.alert();
     }
 
     /**
-     * 구독한 블로그의 RSS에서 게시글들을 읽어서 DB에 저장한다.<br>
-     * 비동기 처리되어 있으며 블로그 하나당 하나의 스레드에서 동작한다.
+     * 구독한 블로그의 RSS에서 게시글들을 읽어서 DB에 저장한다.<br> 비동기 처리되어 있으며 블로그 하나당 하나의 스레드에서 동작한다.
+     *
      * @param subscribe 구독한 블로그
      */
     private void processPostCollectionAsync(SubscribeEntity subscribe) {
-        taskExecutor.execute(() -> {
-            postParser.parseRssDocuments(subscribe.getUrl())
+        taskExecutor.execute(() -> postParser.parseRssDocuments(subscribe.getUrl())
                 .ifPresent(resource -> {
                     updateSubscribeTitle(resource, subscribe);
                     savePosts(resource, subscribe);
-                });
-        });
+                }));
     }
 
-    private void updateSubscribeTitle(RssSubscribeResource subscribeResource, SubscribeEntity subscribe) {
+    private void updateSubscribeTitle(RssSubscribeResource subscribeResource,
+            SubscribeEntity subscribe) {
         subscribe.updateTitle(subscribeResource.subscribeTitle());
         subscribeEntityJpaRepository.save(subscribe);
     }
 
     private void savePosts(RssSubscribeResource subscribeResource, SubscribeEntity subscribe) {
-        List<PostEntity> posts = postEntityJpaRepository.findAllBySubscribeOrderByPubDateDesc(subscribe);
+        List<PostEntity> posts = postEntityJpaRepository.findAllBySubscribeOrderByPubDateDesc(
+                subscribe);
+
         Map<String, PostEntity> postMap = convertListToHashSet(posts);
 
         for (RssItemResource itemResource : subscribeResource.itemResources()) {
@@ -74,9 +81,12 @@ public class PostCollectService {
 
             postEntityJpaRepository.save(post);
         }
+        //TODO 새로운 POST가 수집 되면  큐에 구독을 담는다 이것을 이벤트로 발생
+        publisher.publish(subscribe.toDomain());
     }
 
-    private static Map<String, PostEntity> convertListToHashSet(List<PostEntity> postEntities) {
+    private static Map<String, PostEntity> convertListToHashSet
+            (List<PostEntity> postEntities) {
         Map<String, PostEntity> map = new HashMap<>();
 
         for (PostEntity postEntity : postEntities) {
@@ -85,5 +95,4 @@ public class PostCollectService {
 
         return map;
     }
-
 }
