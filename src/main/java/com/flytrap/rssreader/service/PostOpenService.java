57ed552809d19eb06158548f3827afb5bc2bbce6
saehim <saeilee.vxv@gmail.com>
2023-12-04 15:01:08 +0900

    Feat: 포스트 열림 상태 Delete 서비스 구현 (#95)

--- src/main/java/com/flytrap/rssreader/service/PostOpenService.java
+++ src/main/java/com/flytrap/rssreader/service/PostOpenService.java
@@ -4,6 +4,7 @@ import com.flytrap.rssreader.infrastructure.repository.PostOpenRepository;
 import com.flytrap.rssreader.service.dto.PostOpenParam;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,4 +29,10 @@ public class PostOpenService {
     public Map<Long, Integer> countOpens(long id, List<Long> subscribes) {
         return postOpenRepository.countsGroupBySubscribeId(id, subscribes);
     }
+
+    @Transactional
+    public void deleteRead(long memberId, Long postId) {
+        postOpenRepository.deleteByMemberIdAndPostId(memberId, postId);
+    }
+
 }
