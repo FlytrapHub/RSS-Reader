d8e4b44eee6d434eb6e612183b806c8113f654cc
gamja <leekhy02@gmail.com>
2023-12-04 23:38:53 +0900

    feat: 슬렉 알람시 folder의이름 보이도록 인자에 name추가 (#100)

--- src/main/java/com/flytrap/rssreader/service/alert/AlertFacadeService.java
+++ src/main/java/com/flytrap/rssreader/service/alert/AlertFacadeService.java
@@ -1,8 +1,9 @@
 package com.flytrap.rssreader.service.alert;
 
+import com.flytrap.rssreader.domain.alert.SubscribeEvent;
 import com.flytrap.rssreader.domain.alert.q.SubscribeEventQueue;
-import com.flytrap.rssreader.domain.subscribe.Subscribe;
 import com.flytrap.rssreader.infrastructure.entity.alert.AlertEntity;
+import com.flytrap.rssreader.service.FolderReadService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ public class AlertFacadeService {
 
     private final SubscribeEventQueue queue;
     private final AlertService alertService;
+    private final FolderReadService folderReadService;
 
     @Async("taskScheduler")
     @Scheduled(fixedRate = 1000)
@@ -25,13 +27,14 @@ public class AlertFacadeService {
         //TODO:큐를 확인해 큐의 Subscribe로 구독된 Folder를 찾는다. -> Alert를 찾는다 -> Member에게 알림을 고한다.
         //TODO: ex) 만약 0번 Member가 6번 Folder를 Subscribe 중이라면 이때 Slack(1)으로 알람 받기를 원하는 경우
         if (queue.isRemaining()) {
-            Subscribe subscribe = queue.poll();
-            log.info("alert 스케쥴러 실행 eventQueue poll 의 상태 = {}  ", subscribe.toString());
-            List<AlertEntity> alertList = alertService.getAlertList(subscribe.getId());
-
-            for (AlertEntity alertEntity : alertList) {
-                log.info("alert 정보 = {} ", alertEntity);
-                alertService.notifyAlert(subscribe.getUrl(), subscribe.getTitle());
+            SubscribeEvent event = queue.poll();
+            log.info("alert 스케쥴러 실행 eventQueue poll 의 상태 = {}  ", event.toString()); //새로운 게시글들
+            List<AlertEntity> alertList = alertService.getAlertList(event.subscribeId());
+            if (!alertList.isEmpty()) {
+                alertList.forEach(alertEntity -> {
+                alertService.notifyAlert(event.posts(),
+                            folderReadService.findById(alertEntity.getFolderId()).getName());
+                });
             }
         }
     }
