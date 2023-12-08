011fc4df6b4e902abac5092b939da9901ef5e85e
gamja <leekhy02@gmail.com>
2023-12-08 23:40:52 +0900

    test: scouter agent 설정 형식 (#111)

---
+++ run.sh
@@ -0,0 +1,3 @@
+java -javaagent:/c/Users/vugil/scouter/agent.java/scouter.agent.jar \
+-Dscouter.config=/c/Users/vugil/scouter/agent.java/conf/scouter.conf \
+-jar "/c/spring_framework/rss-reader/build/libs/rss-reader-0.0.1-SNAPSHOT.jar"
