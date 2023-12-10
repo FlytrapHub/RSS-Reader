3eb6b5044a6b812ebaab35a4067bc432df24cfbb
gamja <leekhy02@gmail.com>
2023-12-10 18:39:15 +0900

    test: 명령어 추가 (#111)

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 java -javaagent:/c/Users/vugil/scouter/agent.java/scouter.agent.jar \
 -Dscouter.config=/c/Users/vugil/scouter/agent.java/conf/scouter.conf \
 -jar "/c/spring_framework/rss-reader/build/libs/rss-reader-0.0.1-SNAPSHOT.jar"
+
+#ngrinder명령어
+java  -jar ngrinder-controller-3.5.8.war --port 7070
