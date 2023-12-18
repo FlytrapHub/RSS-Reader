26beec9acdbd9f6984ccd2673913162d74bc7bf9
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-12-18 17:25:15 +0900

    remove: PostOpenRepository 필요없는 코드 제거

--- src/main/java/com/flytrap/rssreader/infrastructure/repository/PostOpenRepository.java
+++ src/main/java/com/flytrap/rssreader/infrastructure/repository/PostOpenRepository.java
@@ -12,25 +12,12 @@ public interface PostOpenRepository extends JpaRepository<OpenEntity, Long> {
     boolean existsByMemberIdAndPostId(Long memberId, Long postId);
 
     @Query("select p.subscribe.id as subscribeId, count(o.postId) as postCount "
-            + "from OpenEntity o "
-            + "inner join PostEntity p on o.postId = p.id "
-            + "where o.memberId = :id and p.subscribe.id in :subscribes "
-            + "group by o.postId")
+        + "from OpenEntity o "
+        + "inner join PostEntity p on o.postId = p.id "
+        + "where o.memberId = :id and p.subscribe.id in :subscribes "
+        + "group by o.postId")
     List<OpenPostCount> countOpens(@Param("id") long id,
-            @Param("subscribes") List<Long> subscribes);
-
-    default Map<Long, Integer> countsGroupBySubscribeId(long id, List<Long> subscribes) {
-        List<OpenPostCount>resultList = countOpens(id, subscribes);
-
-        Map<Long, Integer> result = new HashMap<>();
-        for (OpenPostCount[] row : resultList) {
-            Long subscribeId = (Long) row[0];
-            Integer postCount = ((Number) row[1]).intValue(); // count(p)는 Number 타입이므로 적절한 형변환 필요
-            result.put(subscribeId, postCount);
-        }
-
-        return result;
-    }
+        @Param("subscribes") List<Long> subscribes);
 
     int deleteByMemberIdAndPostId(long memberId, Long postId);
 }
