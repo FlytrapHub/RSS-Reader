e49ba84315d14651dd5a74d5836ddc405b46e687
gamja <leekhy02@gmail.com>
2023-12-18 23:53:27 +0900

    feat: all메서드추가 (#136)

--- src/main/java/com/flytrap/rssreader/service/SubscribeService.java
+++ src/main/java/com/flytrap/rssreader/service/SubscribeService.java
@@ -57,4 +57,7 @@ public class SubscribeService {
                 .toList();
     }
 
+    public List<SubscribeEntity> findSubscribeList() {
+        return subscribeRepository.findAll();
+    }
 }
