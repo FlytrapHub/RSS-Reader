44a04f0120deddd6ddc53275c4fd9ec7bba3f418
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-01-09 23:27:51 +0900

    fix: 로그인시 반환되는 회원정보에 name, profile 추가

--- src/main/java/com/flytrap/rssreader/presentation/controller/api/AuthControllerApi.java
+++ src/main/java/com/flytrap/rssreader/presentation/controller/api/AuthControllerApi.java
@@ -2,14 +2,14 @@ package com.flytrap.rssreader.presentation.controller.api;
 
 import com.flytrap.rssreader.global.model.ApplicationResponse;
 import com.flytrap.rssreader.presentation.dto.Login;
-import com.flytrap.rssreader.presentation.dto.SessionMember;
+import com.flytrap.rssreader.presentation.dto.LoginResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthControllerApi {
 
     // TODO: Swaager 어노테이션 붙여주세요.
-    ApplicationResponse<SessionMember> login(@RequestBody Login request,
+    ApplicationResponse<LoginResponse> login(@RequestBody Login request,
         HttpSession session);
 
     // TODO: Swaager 어노테이션 붙여주세요.
