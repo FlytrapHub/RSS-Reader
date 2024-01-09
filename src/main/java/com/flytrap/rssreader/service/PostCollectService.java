cfae11e7f8666bc053bbdb9f7bc25311db2b6428
gamja <leekhy02@gmail.com>
2024-01-09 17:41:45 +0900

    feat: 스레드 로그 추가, 비동기 어노테이션 추가 (#136)

--- src/main/java/com/flytrap/rssreader/service/PostCollectService.java
+++ src/main/java/com/flytrap/rssreader/service/PostCollectService.java
@@ -1,7 +1,6 @@
 package com.flytrap.rssreader.service;
 
 import com.flytrap.rssreader.domain.post.q.PostBulkInsertPublisher;
-import com.flytrap.rssreader.domain.post.q.PostBulkInsertQueue;
 import com.flytrap.rssreader.infrastructure.api.RssPostParser;
 import com.flytrap.rssreader.infrastructure.api.dto.RssSubscribeResource;
 import com.flytrap.rssreader.infrastructure.api.dto.RssSubscribeResource.RssItemResource;
@@ -9,12 +8,14 @@ import com.flytrap.rssreader.infrastructure.entity.post.PostEntity;
 import com.flytrap.rssreader.infrastructure.entity.subscribe.SubscribeEntity;
 import com.flytrap.rssreader.infrastructure.repository.PostEntityJpaRepository;
 import com.flytrap.rssreader.infrastructure.repository.SubscribeEntityJpaRepository;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -26,18 +27,13 @@ public class PostCollectService {
     private final RssPostParser postParser;
     private final SubscribeEntityJpaRepository subscribeEntityJpaRepository;
     private final PostEntityJpaRepository postEntityJpaRepository;
-    private final PostBulkInsertQueue bulkInsertQueue;
     private final PostBulkInsertPublisher publisher;
 
-    /**
-     * 구독한 블로그의 RSS에서 게시글들을 읽어서 DB에 저장한다.<br> 비동기 처리되어 있으며 블로그 하나당 하나의 스레드에서 동작한다.
-     *
-     * @param subscribe 구독한 블로그
-     * @return
-     */
-
+    @Async("asyncTaskExecutor")
     public CompletableFuture<Map<String, String>> processPostCollectionAsync(
             SubscribeEntity subscribe) {
+        log.info(" task1 with " + Thread.currentThread().getName() + " at time: "
+                + LocalDateTime.now());
         return CompletableFuture.supplyAsync(
                 () -> postParser.parseRssDocuments(subscribe.getUrl())
                         .map(resource -> {
@@ -73,7 +69,7 @@ public class PostCollectService {
                 postUrlMap.put(post.getGuid(), post.getTitle());
             }
             publisher.publish(post);
-            log.info("bulkInsertQueue.size() = {}", bulkInsertQueue.size());
+//            log.info("bulkInsertQueue.size() = {}", bulkInsertQueue.size());
             //     postEntityJpaRepository.save(post);
         }
         return postUrlMap;
