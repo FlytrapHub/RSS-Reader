4d88f9d5cd0bc7ca78bf5367f665c4c6435f11d4
gamja <leekhy02@gmail.com>
2024-01-16 19:55:21 +0900

    chore: 더미 데이터추가용 프로시저와 쿼리실행 계획 추가  (#136)

--- src/main/resources/db/explain.sql
+++ src/main/resources/db/explain.sql
@@ -1,3 +1,4 @@
+###offset 버전1
 explain
 select distinct p1_0.id,
                 p1_0.subscribe_id,
@@ -48,7 +49,7 @@ order by p1_0.pub_date desc
     limit 400,15;
 
 
-
+###nooffset where없이,Join없이
 explain
 SELECT p.id,
        p.description,
@@ -68,48 +69,49 @@ ORDER BY p.pub_date DESC, `id` DESC
     LIMIT 15;
 
 
+###nooffset 커스텀 커서방식 인덱스 있음
 explain
-select
-    distinct p1_0.id,
-             p1_0.subscribe_id,
-             p1_0.guid,
-             p1_0.title,
-             p1_0.thumbnail_url,
-             p1_0.description,
-             p1_0.pub_date,
-             s2_0.title,
-             o1_0.id is not null,
-             b1_0.id is not null,
-             CONCAT(LPAD(TO_SECONDS(p1_0.pub_date), 12, '0'), LPAD(p1_0.id, 8, '0')) AS `cursor`
+select distinct p1_0.id,
+                p1_0.subscribe_id,
+                p1_0.guid,
+                p1_0.title,
+                p1_0.thumbnail_url,
+                p1_0.description,
+                p1_0.pub_date,
+                s2_0.title,
+                o1_0.id is not null,
+                b1_0.id is not null,
+                CONCAT(LPAD(TO_SECONDS(p1_0.pub_date), 12, '0'), LPAD(p1_0.id, 8, '0')) AS `cursor`
     #             CONCAT(LPAD(UNIX_TIMESTAMP(p1_0.pub_date), 10, '0'), LPAD(p1_0.id, 8, '0')) AS `cursor`
-from
+FROM
     rss_post p1_0
-        join
-    rss_subscribe s2_0
-    on s2_0.id=p1_0.subscribe_id
-        left join
-    open o1_0
-    on p1_0.id=o1_0.post_id
-        left join
-    bookmark b1_0
-    on p1_0.id=b1_0.post_id
-        join
-    rss_subscribe s1_0
-    on p1_0.subscribe_id=s1_0.id
-        join
-    folder_subscribe f1_0
-    on s1_0.id=f1_0.subscribe_id
-        join
-    folder f2_0
-    on f1_0.folder_id=f2_0.id
-        left join
-    folder_member s3_0
-    on f2_0.id=s3_0.folder_id
-WHERE CONCAT(LPAD(TO_SECONDS(p1_0.pub_date), 12, '0'), LPAD(p1_0.id, 8, '0')) < '06387243182900000482'
-order by
-    p1_0.pub_date desc,
+        JOIN rss_subscribe s2_0 ON s2_0.id = p1_0.subscribe_id
+        LEFT JOIN open o1_0 ON p1_0.id = o1_0.post_id AND o1_0.member_id = 1
+        LEFT JOIN bookmark b1_0 ON p1_0.id = b1_0.post_id AND b1_0.member_id = 1
+        JOIN rss_subscribe s1_0 ON p1_0.subscribe_id = s1_0.id
+        JOIN folder_subscribe f1_0 ON s1_0.id = f1_0.subscribe_id # 복합인덱스
+        JOIN folder f2_0 ON f1_0.folder_id = f2_0.id
+    LEFT JOIN folder_member s3_0 ON f2_0.id = s3_0.folder_id AND s3_0.member_id = 1
+WHERE
+
+    CONCAT(LPAD(TO_SECONDS(p1_0.pub_date), 12, '0'), LPAD(p1_0.id, 8, '0')) <
+    '9999999999999999999'
+  and (
+    (
+    f2_0.member_id=1
+   or s3_0.member_id=1
+    )
+  and (
+    o1_0.member_id=1
+   or o1_0.member_id is null
+    )
+  and (
+    b1_0.member_id=1
+   or b1_0.member_id is null
+    )
+    )
+order by p1_0.pub_date desc,
     p1_0.id desc
-    limit
-    15
+    limit 15
 
 
