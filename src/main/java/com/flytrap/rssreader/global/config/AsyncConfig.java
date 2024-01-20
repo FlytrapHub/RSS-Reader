fc9e9904b943895a4ed2c00864fcbcac299986dc
gamja <leekhy02@gmail.com>
2024-01-21 07:18:34 +0900

    chore:  스레드 풀 갯수 변경합니다 1개로 설정시 속도 저하  (#136)

--- src/main/java/com/flytrap/rssreader/global/config/AsyncConfig.java
+++ src/main/java/com/flytrap/rssreader/global/config/AsyncConfig.java
@@ -20,8 +20,8 @@ public class AsyncConfig {
     @Bean(name = "asyncTaskExecutor")
     public ThreadPoolTaskExecutor asyncTaskExecutor () {
         ThreadPoolTaskExecutor  executor  =  new  ThreadPoolTaskExecutor ();
-        executor.setCorePoolSize( 1 ); // 풀의 초기 스레드 수를 설정합니다.
-        executor.setMaxPoolSize( 5 ); // 풀의 최대 스레드 수를 설정합니다.
+        executor.setCorePoolSize( 5 ); // 풀의 초기 스레드 수를 설정합니다.
+        executor.setMaxPoolSize( 15 ); // 풀의 최대 스레드 수를 설정합니다.
       //  executor.setQueueCapacity( 20 ); // 보류 중인 작업을 보관하기 위한 대기열 용량 설정
         executor.setThreadNamePrefix( "AsyncTask-" ); // 스레드 이름에 대한 접두사 설정
         executor.initialize();
