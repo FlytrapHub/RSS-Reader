c4217c878f0ee397e183f0a6992ab5d7218e24de
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-02-06 17:42:49 +0900

    refactor: BlogPlatform 필요없는 주석, import문 제거 (#193)

--- src/main/java/com/flytrap/rssreader/domain/subscribe/BlogPlatform.java
+++ src/main/java/com/flytrap/rssreader/domain/subscribe/BlogPlatform.java
@@ -1,8 +1,6 @@
 package com.flytrap.rssreader.domain.subscribe;
 
-import com.flytrap.rssreader.global.exception.ApplicationException;
 import com.flytrap.rssreader.global.exception.NoSuchBlogPlatformException;
-import java.io.NotSerializableException;
 import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +11,10 @@ public enum BlogPlatform {
     VELOG, TISTORY, GITPAGE, ETC;
 
     public static BlogPlatform parseLink(String link) {
-        // 정규표현식을 사용하여 도메인 추출
+
         return Arrays.stream(BlogPlatform.values())
                 .filter(blogPlatform -> link.toUpperCase().contains(blogPlatform.name()))
                 .findFirst()
-                //TODO 일치하는 패턴이 없을 경우 예외 처리 또는 기본값 설정 , 요거 예외처리 해야함
                 .orElseThrow(() -> new NoSuchBlogPlatformException(link));
     }
 }
