47fef253725eecb133e2a7ed722c974c7bb92ddc
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-02-08 04:04:42 +0900

    fix: BlogPlatform Medium 플랫폼 추가 및 getter 메서드 제거 (#193)

--- src/main/java/com/flytrap/rssreader/domain/subscribe/BlogPlatform.java
+++ src/main/java/com/flytrap/rssreader/domain/subscribe/BlogPlatform.java
@@ -1,6 +1,5 @@
 package com.flytrap.rssreader.domain.subscribe;
 
-import com.flytrap.rssreader.global.exception.NoSuchBlogPlatformException;
 import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +9,10 @@ import lombok.RequiredArgsConstructor;
 public enum BlogPlatform {
     VELOG("v2.velog.io/rss/"),
     TISTORY(".tistory.com/rss"),
+    MEDIUM("medium.com/feed/"),
     ETC(" 기타");
 
     private final String signatureUrl;
-
-    public String getSignatureUrl() {
-        return signatureUrl;
-    }
 
     public static BlogPlatform parseLink(String url) {
 
